67cbacda4b70f55415687bb5d45004ffe3b68f5c
Seb Parkitny <parkitny@gmail.com>
2023-05-10 15:29:29 +0930

    Fix warning about (step) must be strictly increasing for series attribute: metrics/precision. Invalid point: 1.0

--- ultralytics/yolo/utils/callbacks/neptune.py
+++ ultralytics/yolo/utils/callbacks/neptune.py
@@ -21,7 +21,7 @@ def _log_scalars(scalars, step=0):
     """Log scalars to the NeptuneAI experiment logger."""
     if run:
         for k, v in scalars.items():
-            run[k].append(value=v, step=step)
+            run[k].append(value=v)
 
 
 def _log_images(imgs_dict, group=''):
